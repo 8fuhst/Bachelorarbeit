ad9b4cc23b715e3277c1bd9551642d0da1f71ba9
8fuhst <59779937+8fuhst@users.noreply.github.com>
2022-01-29 16:14:08 +0100

    Fixed DB integration

--- backend/src/main/java/com/example/controller/TypingDataController.java
+++ backend/src/main/java/com/example/controller/TypingDataController.java
@@ -2,7 +2,7 @@ package com.example.controller;
 
 import com.example.model.TypingData;
 import com.example.repositories.TypingDataRepository;
-import com.google.gson.Gson;
+import com.google.gson.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,12 +20,15 @@ public class TypingDataController {
         System.out.println("td_json: " + td_json);
         if(!(td_json.equals("")) && td_json != null) {
             System.out.println("Received Data!");
-            Gson g = new Gson(); //TODO: Fix!!
-            TypingData td = g.fromJson(td_json, TypingData.class);
+            JsonObject td = new Gson().fromJson(td_json, JsonObject.class);
             typingRepo.insert(td);
         }
         else {
             System.out.println("Error!");
         }
+    }
+
+    private JsonElement parse(String json) throws JsonSyntaxException {
+        return new JsonParser().parse(json).getAsJsonObject();
     }
 }
